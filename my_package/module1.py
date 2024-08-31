66f0aaa514ff2d8f4458c95a8a6b03fe4649bee1
ABDELLAH-Hallou <abdeallahhallou33@gmail.com>
2024-08-31 16:09:50 +0100

    my_package

---
+++ my_package/module1.py
@@ -0,0 +1,6 @@
+class Hello:
+    def __init__(self, name):
+        self.name = name
+    
+    def greet(self):
+        return f"Hello, {self.name}!"
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="my_package",
+    version="0.1",
+    include_package_data=True,
+    python_requires='>=3.8',
+    packages=find_packages(),
+    setup_requires=['setuptools-git-versioning'],
+    install_requires=requirements,
+    author="Abdellah HALLOU",
+    author_email="abdeallahhallou33@gmail.com",
+    description="A short description of your package",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    version_config={
+       "dirty_template": "{tag}",
+    }
+)
